e65332bca8d6f998356f8e3faf07a3cc7ac1a7eb
Rhymesdavi <dokeyo390@gmail.com>
2025-07-04 14:52:08 +0300

    Implement AI background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
-"use client"
-import { Button } from "@/components/ui/button"
-import {useTRPC} from "@/trpc/client";
-import {useMutation} from "@tanstack/react-query";
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
+import { toast } from "sonner";
 const Page = () => {
-    const trpc = useTRPC()
-    const invoke = useMutation(trpc.invoke.mutationOptions({}))
+  const [value, setValue] = useState("")
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Backgrounf job started!")
+    }
+  }));
   return (
-    <div className='p-4 max-w-7xl mx-auto'>
-        <Button onClick={() => invoke.mutate({ text: "Test" })}>Invoke Background Job</Button>
+    <div className="p-4 max-w-7xl mx-auto">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} />
+      <Button onClick={() => invoke.mutate({ value: value })}>
+        Invoke Background Job
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,18 @@
+import { Agent, openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
-    { id: "hello-world" },
-    { event: "test/hello.world" },
-    async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "1s");
-        return { message: `Hello ${event.data.email}!` };
-    },
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event }) => {
+    const summarizer = createAgent({
+      name: "summarizer",
+      system:
+        "You are an expert summarizer.  YYou summarize in 2 words.",
+      model: openai({ model: "gpt-4o" }),
+    });
+    const { output } = await summarizer.run(`Summarize the following text: ${event.data.value}`);
+
+    return { output }
+  }
 );
